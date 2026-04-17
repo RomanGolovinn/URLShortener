4fde14b38de9c3bf96a553c22b5e8c3b0ff94755
Roman Golovin <golovinroman.ev@gmail.com>
2026-04-17 12:29:08 +0300

    feat: started the logic, generated a random code for a link

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,37 @@
 package service
+
+import (
+	"crypto/rand"
+	"math/big"
+)
+
+const (
+	alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+)
+
+type Generator interface {
+	Generate() (string, error)
+}
+
+type RandomGenerator struct {
+	length int
+}
+
+func NewRandomGenerator(length int) *RandomGenerator {
+	return &RandomGenerator{length: length}
+}
+
+func (r *RandomGenerator) Generate() (string, error) {
+	short := make([]byte, r.length)
+	alphabetLen := big.NewInt(int64(len(alphabet)))
+
+	for i := 0; i < r.length; i++ {
+		n, err := rand.Int(rand.Reader, alphabetLen)
+		if err != nil {
+			return "", err
+		}
+		short[i] = alphabet[n.Int64()]
+	}
+
+	return string(short), nil
+}
